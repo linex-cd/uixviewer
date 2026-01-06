a79dbfd08b123b860169370324db31ede8c442fb
Linex <26711215+linex-cd@users.noreply.github.com>
2026-01-06 21:22:47 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -37,7 +37,7 @@ export default function UIXViewer() {
     const imgUrl = params.get('img') || params.get('screenshot');
 
     if (uixUrl || imgUrl) {
-      loadFromUrls(uixUrl, imgUrl, false); // 初始加载不保存历史
+      loadFromUrls(uixUrl, imgUrl);
     }
   }, []);
 
